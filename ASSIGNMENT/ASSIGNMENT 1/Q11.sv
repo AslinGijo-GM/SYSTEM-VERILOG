7ed63600495bb7caeb826939c157f1d27fdfe6bd
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-03-13 08:49:05 +0530

    Update Q11.sv

--- ASSIGNMENT/ASSIGNMENT 1/Q11.sv
+++ ASSIGNMENT/ASSIGNMENT 1/Q11.sv
@@ -4,13 +4,9 @@ module dimm;
   
   initial begin
     
-    foreach( dim[i,j] ) begin
-      
-      my_array1[4][30] = 1'b1;    //not out bound
-      my_array1[29][4] = 1'b1;    // out bound
-      my_array1[4] = 31'b1;       //not valid
-      
-    end
+      dim[4][30] = 1'b1;    //not out bound
+      dim[29][4] = 1'b1;    // out bound
+      dim[4] = 31'b1;       //not valid
     
   end
   
